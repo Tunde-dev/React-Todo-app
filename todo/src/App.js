86de99dbc37566b04ee6b9ac2fa75712bddb14fe
Veres Tünde <tunderke86@gmail.com>
2021-06-10 12:01:01 +0200

    added dashboard logic

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,47 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import Dashboard from "./components/Dashboard";
 
 function App() {
+  useEffect(() => {
+    document.title = "ToDo List";
+  });
+
+  const [dashboards, setDashboards] = useState([
+    {
+      id: "dash-1",
+      name: "Untilted",
+    },
+  ]);
+
+  const addDashboardHandler = (e) => {
+    e.preventDefault();
+    setDashboards([
+      ...dashboards,
+      { id: `dash-${dashboards.length + 1}`, name: "Untilted" },
+    ]);
+  };
+
   return (
-    <div className="App">
-      
+    <div className="d-flex coloumn align-center">
+      <h1>ToDo App</h1>
+      <div className="dashboard-list">
+        {dashboards.map((dashboard) => (
+          <Dashboard
+            key={dashboard.id}
+            name={dashboard.name}
+            dashboard={dashboard}
+            dashboards={dashboards}
+            setDashboards={setDashboards}
+          />
+        ))}
+
+        <button onClick={addDashboardHandler} className="add-dashboard">
+          <FontAwesomeIcon icon={faPlus} /> New dashboard
+        </button>
+      </div>
     </div>
   );
 }
--- todo/src/components/Dashboard.js
+++ todo/src/components/Dashboard.js
@@ -1,6 +1,75 @@
-const Dashboard = () => {
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faPlus } from "@fortawesome/free-solid-svg-icons";
+
+const Dashboard = ({
+  dashboard,
+  dashboards,
+  setDashboards,
+  name,
+  cards,
+  setCards,
+}) => {
+  const [isEditable, setEditable] = useState(false);
+
+  const [newName, setNewName] = useState(name);
+
+  const addCardHandler = () => {
+    setCards([
+      ...cards,
+      {
+        id: `card-${cards.length + 1}`,
+        dashboard: dashboard.id,
+        name: "Task",
+        description: "Description",
+      },
+    ]);
+  };
+
+  const updateDashboardTitle = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const viewTemplate = <h2 className="dashTitleView">{newName}</h2>;
+
+  const editTemplate = (
+    <input
+      className="dashTitle"
+      type="text"
+      autoFocus
+      value={newName}
+      onChange={updateDashboardTitle}
+      onBlur={(e) => setEditable(false)}
+      onFocus={(e) => e.target.select()}
+    />
+  );
+
+  const deleteHandlerDash = () => {
+    setDashboards(dashboards.filter((element) => element.id !== dashboard.id));
+  };
+
   return (
-    <div></div>
+    <div className="dashboard">
+      <div className="dashboard-title-wrapper">
+        <div onClick={(e) => setEditable(true)} className="editable-area">
+          {isEditable ? editTemplate : viewTemplate}
+        </div>
+        <button className="action-button">
+          <FontAwesomeIcon
+            className="new-card plusDashIcon"
+            icon={faPlus}
+            onClick={addCardHandler}
+          />
+        </button>
+        <button className="action-button">
+          <FontAwesomeIcon
+            className="deleteDashIcon"
+            icon={faTrash}
+            onClick={deleteHandlerDash}
+          />
+        </button>
+      </div>
+    </div>
   );
 };
 
